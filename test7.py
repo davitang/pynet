75644112a681086549d879ba5f7c9afe336bcdce
davitang <94767438+davitang@users.noreply.github.com>
2021-11-20 23:25:03 -0600

    Update test7.py

--- test7.py
+++ test7.py
@@ -1,3 +1,10 @@
+four_multiple = 0
+
 for x in range(100):
     if x % 4 == 0:
-        print(x)
+        print("{} is can be divided by 4.".format(x))
+        four_multiple += 1
+ 
+print("you have {} four multiplier in 100".format(four_multiple))
+
+ 
